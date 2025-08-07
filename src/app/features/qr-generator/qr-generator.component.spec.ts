0515573f6f50822afe5acaf9223bf581b93147df
Adriano Sabo <adriano.sabo@ciandt.com>
2025-08-07 11:21:41 -0300

    fix: resolve failing unit tests
    - Add HttpClientTestingModule to QrGeneratorComponent test
    - Add ReactiveFormsModule to support form testing
    - Update AppComponent test to match actual rendered title
    - All tests now passing (4/4 SUCCESS)

--- src/app/features/qr-generator/qr-generator.component.spec.ts
+++ src/app/features/qr-generator/qr-generator.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { QrGeneratorComponent } from './qr-generator.component';
 
@@ -8,7 +10,11 @@ describe('QrGeneratorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [QrGeneratorComponent]
+      imports: [
+        QrGeneratorComponent,
+        HttpClientTestingModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
 
